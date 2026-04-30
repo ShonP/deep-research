86f2a0a047ded1a2a80cade44b81e09d92bbaa8e
Shon Pazarker <pazshon1@gmail.com>
2026-04-30 12:58:18 +0300

    remove unused

--- deep_research/config.py
+++ deep_research/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     reddit_client_id: str = ""
     reddit_client_secret: str = ""
 
-    model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
+    model_config = {"env_file": ".env", "env_file_encoding": "utf-8", "extra": "ignore"}
 
 
 _settings: Settings | None = None
